0bc5ba8c8dd32b663da2933f58616cf083c4e5f3
Valdecy Junior <valdecyjunior.compasso@natura.net>
2026-04-19 22:54:10 -0300

    feat: include workspace in bucket name

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "bucket" {
-  bucket = "bucket-iac-terraform-02648ad7"
+  bucket = "bucket-iac-terraform-${terraform.workspace}-02648ad7"
 
   tags = {
     Name = "Meu Bucket"
