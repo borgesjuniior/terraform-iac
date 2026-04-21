7f4d322ebedde65cf9e267fe1375b5309f74fe96
Valdecy Junior <valdecyjunior.compasso@natura.net>
2026-04-21 20:03:15 -0300

    feat: update default CIDR and subnet variables

--- variables.tf
+++ variables.tf
@@ -12,10 +12,10 @@ variable "vpc" {
 
   default = {
     name               = "vpc"
-    cidr               = "10.1.0.0/16"
+    cidr               = "10.0.0.0/16"
     availability_zones = ["us-east-2a", "us-east-2b", "us-east-2c"]
-    public_subnets     = ["10.1.0.0/24", "10.1.1.0/24", "10.1.2.0/24"]
-    private_subnets    = ["10.1.3.0/24", "10.1.4.0/24", "10.1.5.0/24"]
+    public_subnets     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+    private_subnets    = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   }
 }
 
