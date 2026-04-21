91c07a38e6e28ef55f5832eb6878f4ce61686699
Valdecy Junior <valdecyjunior.compasso@natura.net>
2026-04-21 16:18:36 -0300

    chore: change default name

--- variables.tf
+++ variables.tf
@@ -10,7 +10,7 @@ variable "vpc" {
   description = "VPC configuration object"
 
   default = {
-    name               = "tf-iac-vpc"
+    name               = "vpc"
     cidr               = "10.1.0.0/16"
     availability_zones = ["us-east-2a", "us-east-2b", "us-east-2c"]
     public_subnets     = ["10.1.0.0/24", "10.1.1.0/24", "10.1.2.0/24"]
@@ -29,7 +29,7 @@ variable "ec2" {
   description = "EC2 instance configuration object"
 
   default = {
-    name          = "ubuntu-instance"
+    name          = "ec2-instance"
     ami           = "ami-07062e2a343acc423"
     instance_type = "t3.micro"
     key_name      = null
